1f8cd51a667c69cf03fda1e210eaabbcf9e6fb87
Nuel Elekwachi <elekwaeke300@gmail.com>
2025-02-01 16:20:41 +0100

    web-design page done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,127 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import heroImg from "../../images/home/image-hero-phone.png";
+import webImg from "../../images/home/desktop/image-web-design-large.jpg";
+import appImg from "../../images/home/desktop/image-app-design.jpg";
+import graphicImg from "../../images/home/desktop/image-graphic-design.jpg";
+import passionIcon from "../../images/home/passionate.svg";
+import friendlyIcon from "../../images/home/friendly.svg";
+import resourcefulIcon from "../../images/home/resourceful.svg";
+import rightArrow from "../../svg/icon-right-arrow.svg";
+import { Link } from "react-router";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <section className="bg-[#E7816B] w-auto md:w-[689px] lg:w-7xl mx-auto px-8 h-[45rem] overflow-hidden flex flex-col lg:flex-row md:rounded-2xl mt-5">
+        <div className="text-white text-center lg:text-start mt-40 space-y-5">
+          <h1 className="font-medium text-3xl md:text-5xl lg:w-[30rem]">
+            Award-winning custom designs and digital branding solutions
+          </h1>
+          <p className="lg:w-[26rem]">
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <button className="text-black uppercase bg-white rounded-md px-6 py-2 hover:bg-[#ffad9b] hover:text-white cursor-pointer font-medium text-sm">
+            learn more
+          </button>
+        </div>
+        <div className="">
+          <img src={heroImg} alt="" className="" />
+        </div>
+      </section>
+
+      <section className="my-40 w-auto lg:w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-4">
+        <Link to="/web-design">
+          <div
+            className="bg-cover bg-center bg-no-repeat w-4/5 md:w-[689px] lg:w-[470px] h-60 lg:h-[420px] mx-auto lg:mx-0 rounded-2xl text-white flex justify-center flex-col space-y-6 cursor-pointer relative group"
+            style={{ backgroundImage: `url(${webImg})` }}
+          >
+            <div className="hidden lg:block absolute inset-0 w-[470px] h-[420px] bg-[#e7816b] opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-2xl"></div>
+            <h2 className="uppercase font-medium text-4xl">web design</h2>
+            <p className="uppercase font-medium text-sm tracking-widest flex items-center gap-2 justify-center">
+              view projects
+              <img src={rightArrow} alt="" />
+            </p>
+          </div>
+        </Link>
+
+        <div className="flex flex-col gap-8">
+          <div
+            className="bg-cover bg-center bg-no-repeat w-4/5 md:w-[689px] lg:w-[470px] h-60 md:h-[197px] mx-auto lg:mx-0 rounded-2xl text-white flex justify-center flex-col space-y-6 cursor-pointer relative group"
+            style={{ backgroundImage: `url(${appImg})` }}
+          >
+            <div className="hidden lg:block absolute inset-0 w-[470px] h-[197px] bg-[#e7816b] opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-2xl"></div>
+
+            <h2 className="uppercase font-medium text-4xl">app design</h2>
+            <p className="uppercase font-medium text-sm tracking-widest flex items-center gap-2 justify-center">
+              view projects
+              <img src={rightArrow} alt="" />
+            </p>
+          </div>
+          <div
+            className="bg-cover bg-center bg-no-repeat w-4/5 md:w-[689px] lg:w-[470px] h-60 md:h-[197px] mx-auto lg:mx-0 rounded-2xl text-white flex justify-center flex-col space-y-6 cursor-pointer relative group"
+            style={{ backgroundImage: `url(${graphicImg})` }}
+          >
+            <div className="hidden lg:block absolute inset-0 w-[470px] h-[197px] bg-[#e7816b] opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-2xl"></div>
+
+            <h2 className="uppercase font-medium text-4xl">graphic design</h2>
+            <p className="uppercase font-medium text-sm tracking-widest flex items-center gap-2 justify-center">
+              view projects
+              <img src={rightArrow} alt="" />
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-auto lg:w-7xl mx-auto flex flex-col lg:flex-row gap-4 lg:gap-0 items-center justify-between my-40">
+        <div className="md:flex gap-8 lg:block md:text-start lg:text-center">
+          <img src={passionIcon} alt="" className="mx-auto" />
+          <div className="space-y-5">
+            <h2 className="font-medium my-8 text-xl text-[#333136] uppercase tracking-widest">
+              passionate
+            </h2>
+            <p className="text-center md:text-start lg:text-center w-96 text-[#333136]">
+              Each project starts with an in-depth brand research to ensure we
+              only create products that serve a purpose. We merge art, design,
+              and technology into exciting new solutions.
+            </p>
+          </div>
+        </div>
+
+        <div className="md:flex gap-8 lg:block md:text-start lg:text-center">
+          <img src={resourcefulIcon} alt="" className="mx-auto" />
+          <div className="space-y-5">
+            <h2 className="font-medium my-8 text-xl text-[#333136] uppercase tracking-widest">
+              resourceful
+            </h2>
+            <p className="text-center md:text-start lg:text-center w-96 text-[#333136]">
+              Everything that we do has a strategic purpose. We use an agile
+              approach in all of our projects and value customer collaboration.
+              It guarantees superior results that fulfill our clients’ needs.
+            </p>
+          </div>
+        </div>
+
+        <div className="md:flex gap-8 lg:block md:text-start lg:text-center">
+          <img src={friendlyIcon} alt="" className="mx-auto" />
+          <div className="space-y-5">
+            <h2 className="font-medium my-8 text-xl text-[#333136] uppercase tracking-widest">
+              friendly
+            </h2>
+            <p className="text-center md:text-start lg:text-center w-96 text-[#333136]">
+              We are a group of enthusiastic folks who know how to put people
+              first. Our success depends on our customers, and we strive to give
+              them the best experience a company can provide.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/pages/webdesign.jsx
@@ -0,0 +1,179 @@
+import Navbar from "../../components/Navbar";
+import blogr from "../../images/web-design/image-blogr.jpg";
+import builder from "../../images/web-design/image-builder.jpg";
+import camp from "../../images/web-design/image-camp.jpg";
+import express from "../../images/web-design/image-express.jpg";
+import photon from "../../images/web-design/image-photon.jpg";
+import transfer from "../../images/web-design/image-transfer.jpg";
+import appImg from "../../images/home/desktop/image-app-design.jpg";
+import graphicImg from "../../images/home/desktop/image-graphic-design.jpg";
+import rightArrow from "../../svg/icon-right-arrow.svg";
+import Footer from "../../components/Footer";
+import bgPattern2 from "../../svg/bg-pattern-two-circles.svg";
+
+export default function Webdesign() {
+  return (
+    <>
+      <Navbar />
+
+      <section
+        className="bg-[#e7816b] bg-cover bg-center bg-repeat w-[90%] lg:w-7xl mx-auto h-[252px] md:rounded-2xl text-white flex flex-col items-center justify-center space-y-2"
+        style={{ backgroundImage: `url(${bgPattern2})` }}
+      >
+        <h1 className="font-medium text-5xl">Web Design</h1>
+        <p className="lg:w-96">
+          We build websites that serve as powerful marketing tools and bring
+          memorable brand experiences.
+        </p>
+      </section>
+
+      <section className="w-auto lg:w-7xl mx-auto my-20 flex flex-col lg:flex-row flex-wrap gap-4">
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={express}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              express
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              A multi-carrier shipping website for ecommerce businesses
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={transfer}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              transfer
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              Site for low-cost money transfers and sending money within seconds
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={photon}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              photon
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              A state-of-the-art music player with high-resolution audio and DSP
+              effects
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={builder}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              builder
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              Connects users with local contractors based on their location
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={blogr}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              blogr
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              Blogr is a platform for creating an online blog or publication
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[310px] md:w-[689px] lg:w-[310px] h-auto md:h-[478px] mx-auto lg:mx-0 md:flex items-center lg:block">
+          <img
+            src={camp}
+            alt=""
+            className="rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none lg:rounded-tr-2xl md:w-[339px] lg:w-auto md:rounded-bl-2xl lg:rounded-bl-none"
+          />
+          <div
+            className="bg-[#fdf3f0] cursor-pointer p-4 rounded-bl-2xl rounded-br-2xl md:rounded-tr-2xl 
+            md:rounded-br-2xl md:rounded-bl-none lg:rounded-br-2xl lg:rounded-tr-none lg:rounded-bl-2xl md:h-[310px] lg:h-auto md:flex flex-col justify-center lg:block"
+          >
+            <h2 className="uppercase font-medium text-xl text-[#e7816b] tracking-widest">
+              camp
+            </h2>
+            <p className="text-[#333136] md:w-80 lg:w-auto">
+              Get expert training in coding, data, design, and digital marketing
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-auto lg:w-7xl mx-auto my-20">
+        <div className="flex flex-col lg:flex-row gap-8">
+          <div
+            className="bg-cover bg-center bg-no-repeat w-4/5 md:w-[689px] lg:w-[470px] h-60 md:h-[197px] mx-auto lg:mx-0 rounded-2xl text-white flex justify-center flex-col space-y-6 cursor-pointer relative group"
+            style={{ backgroundImage: `url(${appImg})` }}
+          >
+            <div className="hidden lg:block absolute inset-0 w-[470px] h-[197px] bg-[#e7816b] opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-2xl"></div>
+
+            <h2 className="uppercase font-medium text-4xl">app design</h2>
+            <p className="uppercase font-medium text-sm tracking-widest flex items-center gap-2 justify-center">
+              view projects
+              <img src={rightArrow} alt="" />
+            </p>
+          </div>
+          <div
+            className="bg-cover bg-center bg-no-repeat w-4/5 md:w-[689px] lg:w-[470px] h-60 md:h-[197px] mx-auto lg:mx-0 rounded-2xl text-white flex justify-center flex-col space-y-6 cursor-pointer relative group"
+            style={{ backgroundImage: `url(${graphicImg})` }}
+          >
+            <div className="hidden lg:block absolute inset-0 w-[470px] h-[197px] bg-[#e7816b] opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-2xl"></div>
+
+            <h2 className="uppercase font-medium text-4xl">graphic design</h2>
+            <p className="uppercase font-medium text-sm tracking-widest flex items-center gap-2 justify-center">
+              view projects
+              <img src={rightArrow} alt="" />
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+}
